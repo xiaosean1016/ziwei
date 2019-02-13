0a529b034e469c34191a102b07cf9e89cefd7e4c
xiaosean <xiaosean1016@gmail.com>
2019-02-13 17:51:46 +0800

    vote module

---
+++ application/admin/controller/Vote.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: youdone-dev
+ * Date: 2019/2/13
+ * Time: 11:09
+ */
+
+namespace app\admin\controller;
+
+
+class Vote
+{
+
+}
---
+++ application/common/model/Vote.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: youdone-dev
+ * Date: 2019/2/13
+ * Time: 11:26
+ */
+
+namespace app\common\model;
+
+
+class Vote
+{
+
+}
